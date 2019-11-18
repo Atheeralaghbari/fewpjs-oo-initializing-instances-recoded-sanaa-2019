124d6e66ba0e26567439469d902fc98c6006d610
Atheer Alaghbari <atheeralaghbari61@gmail.com>
2019-11-18 11:23:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 // Write your code here
 class Breakfast{
   
-  constructor()
+  constructor(food,breed){
+    this.food=food;
+    this.drink=drink;
+    
+  }
+}
+
+class Lunch{
+  constructor(salad,soup,drink){
+    this.salad=salad;
+    this.soup=soup;
+    this.drink=drink;
+  }
 }
